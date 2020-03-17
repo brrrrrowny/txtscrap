b69059cf6a6c765405b8d9d094bb352de889669c
brrrrrowny <guoyutian59@gmail.com>
2020-03-17 01:00:39 +0100

    Update txtscrap.py

--- txtscrap.py
+++ txtscrap.py
@@ -5,7 +5,7 @@ work_dir = "path/to/workdir"   #doesnt really matter
 
 for index in range(1, 800): #e.g: 1.txt - 800.txt
     name = "{index}.txt".format(index=index)
-    path = os.path.join('/Users/browny/Documents/pwned/user/', name)   #directroy
+    path = os.path.join('/Users/browny/Documents/pwned/user/', name)   #folder you wanna scan
     with io.open(path, mode="r", encoding="utf-8") as fd:
         for line in fd:
             if "crandell" in line: print(line)   #keyphrase
